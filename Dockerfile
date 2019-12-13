4737ed694cd83a04320937507ec1220d6e1145bc
Abhishek Ray <abhishek.ray@robinhood.com>
2019-12-12 19:53:05 -0800

    Update webapp

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
-FROM ubuntu:latest
+FROM python:3.7-slim-buster
 RUN apt-get update -y
-RUN apt-get install -y python-pip python-dev build-essential
-COPY . /app
+
 WORKDIR /app
+
+COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+
+COPY . /app
+
+EXPOSE 8080
 ENTRYPOINT ["python"]
 CMD ["run.py"]
--- web/__init__.py
+++ web/__init__.py
@@ -3,16 +3,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-API_KEY = "cbdb357849b049ef9bd771a456536e5e"
+API_KEY = ""
 NEWS_API_URL = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=%s" % API_KEY
 
 def get_news_articles():
     """Returns the list of news articles."""
     # fetch news articles from the api
-    response = requests.get(NEWS_API_URL)
-
-    # return the articles in JSON format
-    return response.json()['articles']
+    try:
+        response = requests.get(NEWS_API_URL)
+        # return the articles in JSON format
+        return response.json()['articles']
+    except Exception:
+        return "Hello, world"
 
 
 @app.route('/')
