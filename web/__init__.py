624f1bed4746d9abe5c49284bb90ea5252aa28a9
Abhishek Ray <abhishekray07@gmail.com>
2019-12-14 09:27:33 -0800

    Update code to test

--- web/__init__.py
+++ web/__init__.py
@@ -3,20 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-API_KEY = ""
-NEWS_API_URL = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=%s" % API_KEY
-
-def get_news_articles():
-    """Returns the list of news articles."""
-    # fetch news articles from the api
-    try:
-        response = requests.get(NEWS_API_URL)
-        # return the articles in JSON format
-        return response.json()['articles']
-    except Exception:
-        return "Hello, world"
-
 
 @app.route('/')
 def index():
-    return str(get_news_articles())
+    return "Hello, World!"
